1973c0211b3cbabe3eff78352b7e1a7826873c62
BubbleTrouble14 <ronald.goedeke@outlook.com>
2025-07-23 13:34:15 -0300

    fix: bumped version of nitro modules

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,10 @@ import { NitroModules } from 'react-native-nitro-modules';
 import type { InspireFace as NitroInspireFace } from './InspireFace.nitro';
 import type { AssetManager as NitroAssetManager } from './AssetManager.nitro';
 export * from './InspireFace.nitro';
+export * from './Session.nitro';
+export * from './ImageStream.nitro';
+export * from './ImageBitmap.nitro';
+export * from './AssetManager.nitro';
 export * from './enums';
 export * from './types';
 
